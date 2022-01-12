5d0436bc103d7c444bb83e5e9a10bd3461c23436
Robert C <robert.clarady@gmail.com>
2022-01-11 21:06:05 -0600

    updated app get calls for index and note pages

--- server.js
+++ server.js
@@ -2,19 +2,25 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const { notes } = require('./data/db');
+const notes = require('./data/db');
 const PORT = process.env.PORT || 3001;
-
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.get('/api/notes', (req, res)=>{
+    res.json(notes);
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+  });
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
