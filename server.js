b0ac0846339b1d25b67d14cd0be0478c0d58f215
Robert C <robert.clarady@gmail.com>
2022-01-11 18:56:37 -0600

    added routes files

--- server.js
+++ server.js
@@ -2,5 +2,20 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const { db } = require('./data/db');
+const { notes } = require('./data/db');
 const PORT = process.env.PORT || 3001;
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+app.use(express.static('public'));
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
