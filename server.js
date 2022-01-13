c59bbe08e9bc917dc67ffc2a122add36185e7599
Robert C <robert.clarady@gmail.com>
2022-01-13 06:49:42 -0600

    updated routes so all * return to index.html

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 //sets initial value for note id
 var counterID = savedNotes.length;  
 
-//determines which note to delete from db and removes
+//determines which note to delete from db based on note ID and removes
 function checkNote (id) {
   for (i=0; i<savedNotes.length; i++) {
     if (savedNotes[i].id == id) {
@@ -32,7 +32,8 @@ app.use(express.json());
 //listens for get api request and returns notes stored in db
 app.get('/api/notes', (req, res)=>{
     res.json(savedNotes);
-})
+});
+
 //listens for post request and adds a new note to the db
 app.post('/api/notes', (req, res)=>{
   req.body.id = counterID;  //sets note ID based on counter
@@ -40,23 +41,30 @@ app.post('/api/notes', (req, res)=>{
   updateNotes();
   counterID ++; //updates counter to avoid duplicate IDs
   res.json(savedNotes);
-})
+});
+
 //listens for delete request and removes note that matches selected ID
 app.delete('/api/notes/', (req, res)=>{
   let value = req.query.id;
   checkNote(value);
   updateNotes();
   res.json(savedNotes);
-})
+});
+
 //directs all home requests to the index.html page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
   });
+
 //directs all /notes requests to the notes.html page
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
   });
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+  
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
